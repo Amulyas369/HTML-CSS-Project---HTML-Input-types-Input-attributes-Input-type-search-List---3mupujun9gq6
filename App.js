27b4d2b02cf5b41d1b4ab5a658c6215d850f5a19
Amulyas369 <amulyafood369@gmail.com>
2022-09-27 05:22:12 +0530

    please ho jao

--- App.js
+++ App.js
@@ -1,1 +1,12 @@
-
+const codes = document.querySelectorAll('.otp')
+codes[0].focus()
+codes.forEach((otp, idx) => {
+   otp.addEventListener('keydown', (e) => {
+       if(e.key >= 0 && e.key <=9) {
+           codes[idx].value = ''
+           setTimeout(() => codes[idx + 1].focus(), 10)
+       } else if(e.key === 'Backspace') {
+           setTimeout(() => codes[idx - 1].focus(), 10)
+       }
+   })
+})
