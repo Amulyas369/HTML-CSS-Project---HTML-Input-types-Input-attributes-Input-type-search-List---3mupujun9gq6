486eb02913ba7797637184f68ac751ad67ec8bca
Amulyas369 <amulyafood369@gmail.com>
2022-10-03 10:44:51 +0530

    Please ho jao

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
-const codes = document.querySelectorAll('.otp')
+const codes = document.querySelectorAll('.code')
 codes[0].focus()
-codes.forEach((otp, idx) => {
-   otp.addEventListener('keydown', (e) => {
+codes.forEach((code, idx) => {
+   code.addEventListener('keydown', (e) => {
        if(e.key >= 0 && e.key <=9) {
            codes[idx].value = ''
            setTimeout(() => codes[idx + 1].focus(), 10)
